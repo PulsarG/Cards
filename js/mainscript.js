ed1a46f622a2f26cbb22832f51efc898b45d1a42
PulsarG <furylonzero@gmail.com>
2022-04-09 18:42:31 +0500

    firstcommit

---
+++ js/mainscript.js
@@ -0,0 +1,8 @@
+function ShowTwoBtnText(Element) {
+    Element.style.backgroundColor = 'white';
+}
+
+function HiddenTwoBtnText(n) {
+    s = document.getElementById(n).style;
+    s.backgroundColor = 'black';
+}
