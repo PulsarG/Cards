e577d9d91c5119d7359d753a0b48d96501007a52
PulsarG <furylonzero@gmail.com>
2022-04-09 19:46:30 +0500

    ver01

--- js/mainscript.js
+++ js/mainscript.js
@@ -6,3 +6,11 @@ function HiddenTwoBtnText(n) {
     s = document.getElementById(n).style;
     s.backgroundColor = 'black';
 }
+
+function HiddenLearn (Element) {
+    if (Element.style.opacity == 1) {
+        Element.style.opacity = 0;
+    } else {
+        Element.style.opacity = 1;
+    }
+}
